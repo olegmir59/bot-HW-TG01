bd18a74c7b11cfbc44a8a7c781f4e662adbb7e2a
olegmir59 <udom1@ya.ru>
2025-09-10 23:51:46 +0700

    add weather_api.py

---
+++ weather_api.py
@@ -0,0 +1,23 @@
+import requests
+from config import OPENWEATHERMAP_TOKEN, CITY
+
+
+class WeatherAPI:
+    BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+    def get_weather(self):
+        params = {
+            'q': CITY,
+            'appid': OPENWEATHERMAP_TOKEN,
+            'units': 'metric',  # градусы Цельсия
+            'lang': 'ru'
+        }
+        response = requests.get(self.BASE_URL, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            temp = data['main']['temp']
+            description = data['weather'][0]['description']
+            return f"Погода в {CITY}:\nТемпература: {temp}°C\nОписание: {description}"
+        else:
+            return "Не удалось получить данные о погоде"
